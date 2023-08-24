b3ead60bad000ade7f9ca9fdf42e4144b2fc0408
JadeFarrr Wolf's gf <Farrjade2@gmail.com>
2023-08-24 16:42:20 +0000

    first project

---
+++ Norman.py
@@ -0,0 +1,12 @@
+print ("Welcome to the Resteraunt Rater :)" )
+print("Here are some resteraunts for you to rate")
+resteraunts = ["Dog Haus", " Ramen Tatsunoya","Russell's", "Dirt Dog","Jake's Burgers", "True Food Kitchen"]
+new_resteraunt= (input ( "would you like to add to the list yes for no?"))
+if new_resteraunt == "yes":
+    answer = input("What resteraunt would you like to add to the list?")
+elif new_resteraunt == "no":
+    answer = input("Alright lets rank the existing resteraunts")
+    print ("ITS RANKING TIME")
+
+ Do you like A) Dog Haus or B) Ramen Tatsunoya more? 
+
